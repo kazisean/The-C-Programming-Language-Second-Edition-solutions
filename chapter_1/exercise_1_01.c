6e0c9c099e95b809e7b4a2256ce4ec8e2e4ce7e4
Kazi Sean <keh8423@nyu.edu>
2024-02-17 21:42:36 -0500

    save

--- chapter_1/exercise_1_01.c
+++ chapter_1/exercise_1_01.c
@@ -1,8 +1,10 @@
-#include <stdio.h>
+#include <stdio.h> // include information about the standard library 
 
 /* Exercise 1-1 */
+/* Define a function named main 
+that receives no argument values */
 
 int main (){
-    printf("hello, world \n");
+    printf("hello, world\n");
     return 0; // exit the program run
 }
